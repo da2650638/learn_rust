0c4bfaa514bd881203262b9e343175495b5e836a
DongZe <da2650638@gmail.com>
2023-11-15 20:13:37 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main()
+{
+    let logical: bool = true;
+    let a_float: f64 = 3.1415926;
+    let an_integer = 5i32;
+    let default_float = 3.0;
+    let default_integer = 7;
+    let mut inferred_type = 12;
+    inferred_type = 123123123123123123i64;
+
+    let mutable = true;
+}
